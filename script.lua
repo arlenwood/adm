23b2cb3d3b5be97ffdccf882400a6ca843efaa59
arlenwood <arlenwood89@gmail.com>
2025-04-27 12:14:30 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,12 +1,14 @@
-if game.PrivateServerId ~= "" and game.PrivateServerOwnerId ~= 0 then
-    game.Players.LocalPlayer:Kick("Private server detected, NO BOTS ALLOWED")
+local a=game
+local b=a.PrivateServerId
+local c=a.PrivateServerOwnerId
+local d=a.Players.LocalPlayer
+if b~="" and c~=0 then
+    d:Kick("Private server detected, NO BOTS ALLOWED")
     return
 end
 
-spawn(function()
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c29af06acdb7a80fb239415fd322efe9.lua"))()
-end)
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c29af06acdb7a80fb239415fd322efe9.lua"))()
 
-spawn(function()
+task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/arlenwood/autofarm/refs/heads/main/script.lua"))()
 end)
