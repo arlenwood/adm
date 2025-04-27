5e6414cbc79857b16d0391861f2efbdd99549a88
arlenwood <arlenwood89@gmail.com>
2025-04-27 12:17:58 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,15 @@
-local a, b, c, d = game, game.PrivateServerId, game.PrivateServerOwnerId, game.Players.LocalPlayer
-if b ~= "" and c ~= 0 then
-    d:Kick("Private server detected, NO BOTS ALLOWED")
+local g = game
+local pId = g.PrivateServerId
+local oId = g.PrivateServerOwnerId
+local plr = g.Players.LocalPlayer
+
+if pId ~= "" and oId ~= 0 then
+    plr:Kick("Private server detected, NO BOTS ALLOWED")
     return
 end
 
-loadstring(a:HttpGet("https://api.luarmor.net/files/v3/loaders/c29af06acdb7a80fb239415fd322efe9.lua"))()
+loadstring(g:HttpGet("https://api.luarmor.net/files/v3/loaders/c29af06acdb7a80fb239415fd322efe9.lua"))()
 
-task.wait(5) -- wait 5 seconds for Luarmor to finish setup
+task.wait(5)
 
-loadstring(a:HttpGet("https://raw.githubusercontent.com/arlenwood/autofarm/refs/heads/main/script.lua"))()
+loadstring(g:HttpGet("https://raw.githubusercontent.com/arlenwood/autofarm/refs/heads/main/script.lua"))()
